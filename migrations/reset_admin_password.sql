aef934c51e90526c69a4bb0faf48f8f27b6151a6
duchm245 <duchm245@gmail.com>
2026-06-07 22:12:26 +0700

    chore: remove unused SQL files and migrate docs to docs/ folder

--- migrations/reset_admin_password.sql
+++
@@ -1,8 +0,0 @@
--- Reset mật khẩu admin về: 123456
--- Hash được generate bởi PasswordEncoderGenerator.java của chính project này
-USE motorbike_shop;
-UPDATE user
-SET password = '$2a$10$1Xmt73dPIjrpTIFrU04hx.WCLdIjaV9yrCUN6QbhjbT8ntIRuOS.i'
-WHERE username = 'admin';
-
-SELECT id, username, email FROM user WHERE username = 'admin';
